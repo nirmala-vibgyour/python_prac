102763b20ea7662be8b66f43bc4ad4fd8c28cedd
nirmala-vibgyor <nirmalawrk@gmail.com>
2024-08-15 22:38:51 +0530

    Added all the changes from the tutorial

--- SHIP_TANK/bullet.py
+++ SHIP_TANK/bullet.py
@@ -5,7 +5,7 @@ class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
     def __init__(self, ai_settings, screen, ship):
         """Create a bullet object at the ship's current position."""
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
         # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
